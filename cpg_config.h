9fbf934b0dfc04438bac51e79dfa6ac520dbd8cc
Stefano Bossi <sbossi@voismart.it>
2011-02-11 13:12:42 +0000

    VSFREESW-6 license

--- cpg_config.h
+++ cpg_config.h
@@ -1,3 +1,28 @@
+/*
+ * Closed Process Group (Corosync CPG) failover module for Freeswitch
+ *
+ * Copyright (C) 2010 Voismart SRL
+ *
+ * Authors: Stefano Bossi <sbossi@voismart.it>
+ *
+ * Further Contributors:
+ * Matteo Brancaleoni <mbrancaleoni@voismart.it> - Original idea
+ *
+ * This program cannot be modified, distributed or used without
+ * specific written permission of the copyright holder.
+ *
+ * The source code is provided only for evaluation purposes.
+ * Any usage or license must be negotiated directly with Voismart Srl.
+ *
+ * Voismart Srl
+ * Via Benigno Crespi 12
+ * 20159 Milano - MI
+ * ITALY
+ *
+ * Phone : +39.02.70633354
+ *
+ */
+
 #ifndef CPG_CONFIG_H
 #define CPG_CONFIG_H
 
