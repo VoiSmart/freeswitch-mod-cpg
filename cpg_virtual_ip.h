aeadc7194c538a8c7056d5439073a0e8f5f94033
Stefano Bossi <sbossi@voismart.it>
2011-02-11 15:42:01 +0000

    VSFREESW-6 from profile to virtual_ip

--- cpg_virtual_ip.h
+++ cpg_virtual_ip.h
@@ -22,53 +22,62 @@
  * Phone : +39.02.70633354
  *
  */
-#ifndef CPG_PROFILE_H
-#define CPG_PROFILE_H
+#ifndef CPG_VIRTUAL_IP_H
+#define CPG_VIRTUAL_IP_H
 
 #include <switch.h>
 #include <corosync/cpg.h>
-//#include "mod_cpg.h"
 
 #include "node.h"
+
+#define MAX_SOFIA_PROFILE 8
 
 typedef enum{
     MASTER,
     BACKUP,
     INIT,
     STANDBY
-} profile_state_t;///serve davvero?
+} virtual_ip_state_t;
 
 typedef struct {
-    char name[255];
-    char virtual_ip[16];
+    char name[256];
+    switch_bool_t autorecover;
+} sofia_profile_t;
+
+typedef struct {
+
+/*configuration*/
+    char address[16];
     int netmask;
-    profile_state_t state;
     char device[10];
     char mac[18];
-    switch_thread_t *profile_thread;
-    short int running;
-    cpg_handle_t handle;
     struct cpg_name group_name;
-    int members_number;
     int priority;
+    switch_bool_t autoload;
+    switch_bool_t autorollback;
+    int rollback_delay;
+    sofia_profile_t profiles[MAX_SOFIA_PROFILE];
+
+/*runtime information*/
+    virtual_ip_state_t state;
+    switch_thread_t *virtual_ip_thread;
+    switch_bool_t running;
+    cpg_handle_t handle;
+    int members_number;
     uint32_t node_id;
     uint32_t master_id;
     char runtime_uuid[40];
     uint32_t rollback_node_id;
     node_t *node_list;
     size_t member_list_entries;
-    switch_bool_t autoload;
-    switch_bool_t autorecover;
-    switch_bool_t autorollback;
-    int rollback_delay;
-} profile_t;
+} virtual_ip_t;
 
-profile_t *find_profile_by_name(char *profile_name);
+virtual_ip_t *find_virtual_ip(char *address);
 
 
-switch_status_t profile_start(profile_t *profile);
-switch_status_t profile_stop(profile_t *profile);
-switch_status_t profile_send_sql(profile_t *profile, char *sql);
-switch_status_t profile_send_state(profile_t *profile);
+switch_status_t virtual_ip_start(virtual_ip_t *vip);
+switch_status_t virtual_ip_stop(virtual_ip_t *vip);
+switch_status_t virtual_ip_send_sql(virtual_ip_t *vip, char *sql);
+switch_status_t virtual_ip_send_state(virtual_ip_t *vip);
 
 #endif
